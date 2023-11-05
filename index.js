86a44e03c420f46a51d2dfb93e00b6b6809f12b4
Edgar-Carpinteyro <carpinteyroe07@gmail.com>
2023-11-05 14:57:48 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let heading = document.querySelector('h1');
+
+console.log(heading);
+
+let paragraph = document.getElementById("paragraph");
+console.log(paragraph);
+
+let unOrderedList = document.querySelector('ul');
+console.log(unOrderedList);
+
+let list = document.querySelectorAll("li");
+
+console.log(list);
